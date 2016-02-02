2e9ddfc9296de1b46ca2c636520d4f13f5aa0c57
Bonnie <aalle.allen@gmail.com>
2016-02-01 23:47:10 -0500

    SimpleCalculatorTests

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -12,16 +12,19 @@
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
     /* WORK HERE */
+    return number + 1;
     return number;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
+    return number1 + number2;
     return number1;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
+    return dividend % divisor;
     return 0;
 }
 
